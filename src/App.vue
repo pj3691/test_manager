f720f9e540fa361611713cae3f4b2974284ed60c
Your Name <you@example.com>
2022-06-02 22:41:15 +0800

    new_element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
      <router-view/>
-     <el-button type="primary" :loading="true">加载中</el-button>
   </div>
 </template> <style lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import { component } from 'vue/types/umd'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/LoginView.vue')
+  },
+  {
+    path: '/homepage',
+    name: 'homepage',
+    component: () => import('@/views/home/HomePage.vue')
+  }
+
 ]
 
 const router = new VueRouter({
